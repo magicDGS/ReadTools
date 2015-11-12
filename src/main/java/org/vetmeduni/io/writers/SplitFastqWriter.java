48356cd8dfac65157b1f2e484679a2450c69eb15
Daniel <daniel.gomez.sanchez@homtail.es>
2015-11-12 18:09:55 +0100

    added new FastqWriters for split and a factory for all the writers

---
+++ src/main/java/org/vetmeduni/io/writers/SplitFastqWriter.java
@@ -0,0 +1,108 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Daniel Gómez-Sánchez
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ */
+package org.vetmeduni.io.writers;
+
+import htsjdk.samtools.fastq.FastqRecord;
+import htsjdk.samtools.fastq.FastqWriter;
+import org.vetmeduni.io.FastqPairedRecord;
+
+import java.util.Hashtable;
+import java.util.NoSuchElementException;
+
+/**
+ * Interface for split writer for fastq files
+ *
+ * @author Daniel Gómez-Sánchez
+ */
+public interface SplitFastqWriter extends FastqWriter {
+
+	/**
+	 * The mapping between the identifier and the corresponding FastqWriter. Several identifiers could be associated
+	 * with the same writer
+	 *
+	 * @return the mapping identifier-writer
+	 */
+	public Hashtable<String, ? extends FastqWriter> getMapping();
+
+	/**
+	 * Get the number of times that each of the identifiers' writers was used
+	 *
+	 * @return the map between the identifier and the current report
+	 */
+	public Hashtable<String, Integer> getCurrenCount();
+
+	/**
+	 * Get the report for each identifier (the report could be whatever object
+	 *
+	 * @return the map between the identifier and the current report
+	 */
+	public Hashtable<String, Object> getCurrentReport();
+
+	/**
+	 * Write a FastqRecord into the split fastq writer
+	 *
+	 * @param identifier the identifier for the record
+	 * @param record     the record to write
+	 *
+	 * @throws java.util.NoSuchElementException        if the identifier is not in the mapping (or <code>null</code>)
+	 * @throws java.lang.UnsupportedOperationException if the writer does not allow a FastqRecords
+	 */
+	public void write(String identifier, FastqRecord record)
+		throws NoSuchElementException, UnsupportedOperationException;
+
+	/**
+	 * Write a FastqRecord that includes some information for the identifier, or in the default writer depending on the
+	 * implementation
+	 *
+	 * @param record the record to write
+	 *
+	 * @throws java.util.NoSuchElementException        if the detected identifier is not in the mapping or there are no
+	 *                                                 default writer
+	 * @throws java.lang.UnsupportedOperationException if the writer does noa allow FastqRecords
+	 */
+	public void write(FastqRecord record);
+
+	/**
+	 * Write a FastqPairedRecord into the split fastq writer
+	 *
+	 * @param identifier the identifier for the record
+	 * @param record     the record to write
+	 *
+	 * @throws java.util.NoSuchElementException        if the identifier is not in the mapping (or <code>null</code>)
+	 * @throws java.lang.UnsupportedOperationException if the writer does not allow a FastqPairedRecords
+	 */
+	public void write(String identifier, FastqPairedRecord record)
+		throws NoSuchElementException, UnsupportedOperationException;
+
+	/**
+	 * Write a FastqPairedRecord that includes some information for the identifier, or in the default writer depending
+	 * on the implementation
+	 *
+	 * @param record the record to write
+	 *
+	 * @throws java.util.NoSuchElementException        if the detected identifier is not in the mapping or there are no
+	 *                                                 default writer
+	 * @throws java.lang.UnsupportedOperationException if the writer does noa allow FastqPairedRecords
+	 */
+	public void write(FastqPairedRecord record);
+}
---
+++ src/main/java/org/vetmeduni/io/writers/SplitFastqWriterAbstract.java
@@ -0,0 +1,104 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Daniel Gómez-Sánchez
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ */
+package org.vetmeduni.io.writers;
+
+import htsjdk.samtools.fastq.FastqRecord;
+import htsjdk.samtools.fastq.FastqWriter;
+import org.vetmeduni.io.FastqPairedRecord;
+
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.NoSuchElementException;
+
+/**
+ * Abstract implementation of FastqWriter for whatever implementation
+ *
+ * @author Daniel Gómez-Sánchez
+ */
+public abstract class SplitFastqWriterAbstract implements SplitFastqWriter {
+
+	/**
+	 * Hashtable mapping the identifier and the writer
+	 */
+	protected final Hashtable<String, ? extends FastqWriter> mapping;
+
+	/**
+	 * Hashtable mapping the identifier and how many times you perform a write on it with the methods implemented in
+	 * this class
+	 */
+	protected final Hashtable<String, Integer> counts;
+
+	protected SplitFastqWriterAbstract(Hashtable<String, ? extends FastqWriter> mapping) {
+		this.mapping = mapping;
+		this.counts = new Hashtable<>(mapping.size());
+	}
+
+	@Override
+	public Hashtable<String, ? extends FastqWriter> getMapping() {
+		return mapping;
+	}
+
+	@Override
+	public Hashtable<String, Integer> getCurrenCount() {
+		return counts;
+	}
+
+	@Override
+	public void write(String identifier, FastqRecord record)
+		throws NoSuchElementException, UnsupportedOperationException {
+		FastqWriter writer = this.mapping.get(identifier);
+		if (writer == null) {
+			throw new NoSuchElementException("Identifier " + identifier + " is not included in this writer");
+		}
+		writer.write(record);
+		Integer c = counts.putIfAbsent(identifier, 1);
+		if (c != null) {
+			counts.put(identifier, c + 1);
+		}
+	}
+
+	@Override
+	public void write(String identifier, FastqPairedRecord record)
+		throws NoSuchElementException, UnsupportedOperationException {
+		FastqWriter writer = this.mapping.get(identifier);
+		if (writer == null) {
+			throw new NoSuchElementException("Identifier " + identifier + " is not included in this writer");
+		} else if (writer instanceof PairFastqWriters) {
+			((PairFastqWriters) writer).write(record);
+			Integer c = counts.putIfAbsent(identifier, 1);
+			if (c != null) {
+				counts.put(identifier, c + 1);
+			}
+		} else {
+			throw new UnsupportedOperationException("This writer does not allow writing of FastqPairedRecords");
+		}
+	}
+
+	@Override
+	public void close() {
+		HashSet<FastqWriter> writersSet = new HashSet<>(mapping.values());
+		for (FastqWriter writer : writersSet) {
+			writer.close();
+		}
+	}
+}
