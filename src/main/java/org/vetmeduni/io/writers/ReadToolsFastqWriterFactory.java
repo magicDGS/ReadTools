dd56f1d388f04f35894b48c83116f3593d468536
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2015-11-12 18:09:55 +0100

    added new FastqWriters for split and a factory for all the writers

---
+++ src/main/java/org/vetmeduni/io/writers/ReadToolsFastqWriterFactory.java
@@ -0,0 +1,322 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Daniel Gómez-Sánchez
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ */
+package org.vetmeduni.io.writers;
+
+import htsjdk.samtools.fastq.FastqRecord;
+import htsjdk.samtools.fastq.FastqWriter;
+import htsjdk.samtools.fastq.FastqWriterFactory;
+import htsjdk.samtools.util.Lazy;
+import htsjdk.samtools.util.Log;
+import org.vetmeduni.io.FastqPairedRecord;
+import org.vetmeduni.methods.barcodes.BarcodeDictionary;
+import org.vetmeduni.methods.barcodes.BarcodeMethods;
+import org.vetmeduni.utils.IOUtils;
+import org.vetmeduni.utils.record.FastqRecordUtils;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+/**
+ * FastqWriterFactory for ReadTools
+ *
+ * @author Daniel Gómez-Sánchez
+ */
+public class ReadToolsFastqWriterFactory {
+
+	private final Log logger = Log.getInstance(ReadToolsFastqWriterFactory.class);
+
+	/**
+	 * Discarded suffix for discarded output
+	 */
+	public static final String DISCARDED_SUFFIX = "discarded";
+
+	/**
+	 * The underlying factory
+	 */
+	private final FastqWriterFactory FACTORY;
+
+	/**
+	 * By default, the ouput will be GZIPPED if a prefix is provided; if a file is provided, it is not needed
+	 */
+	private boolean GZIP_OUTPUT = true;
+
+	/**
+	 * Initialize a new factory
+	 */
+	public ReadToolsFastqWriterFactory() {
+		FACTORY = new FastqWriterFactory();
+	}
+
+	/**
+	 * Sets whether or not to use async io (i.e. a dedicated thread per writer.
+	 */
+	public void setUseAsyncIo(final boolean useAsyncIo) {
+		FACTORY.setUseAsyncIo(useAsyncIo);
+	}
+
+	/**
+	 * If true, compute MD5 and write appropriately-named file when file is closed.
+	 */
+	public void setCreateMd5(final boolean createMd5) {
+		FACTORY.setCreateMd5(createMd5);
+	}
+
+	/**
+	 * If <code>true</code> the output generated from a prefix will be gzipped.
+	 */
+	public void setGzipOutput(final boolean gzip) {
+		GZIP_OUTPUT = gzip;
+	}
+
+	/**
+	 * Create a new default writer (using {@link htsjdk.samtools.fastq.FastqWriterFactory#newWriter(java.io.File)})
+	 *
+	 * @param out the file to create the writer from
+	 *
+	 * @return a new instance of the writer
+	 */
+	public FastqWriter newWriterDefault(final File out) {
+		return FACTORY.newWriter(out);
+	}
+
+	/**
+	 * Create a new FastqWriter from a prefix
+	 *
+	 * @param prefix the prefix for the files
+	 *
+	 * @return a new instance of the writer
+	 */
+	public FastqWriter newWriter(String prefix) {
+		return newWriterDefault(new File(IOUtils.makeOutputNameFastqWithDefaults(prefix, GZIP_OUTPUT)));
+	}
+
+	/**
+	 * Create a new pair writer
+	 *
+	 * @param prefix the prefix for the files
+	 *
+	 * @return a new instance of the writer
+	 */
+	public PairFastqWriters newPairWriter(String prefix) {
+		final FastqWriter pair1 = FACTORY
+			.newWriter(new File(IOUtils.makeOutputNameFastqWithDefaults(prefix + "_1", GZIP_OUTPUT)));
+		final FastqWriter pair2 = FACTORY
+			.newWriter(new File(IOUtils.makeOutputNameFastqWithDefaults(prefix + "_2", GZIP_OUTPUT)));
+		Lazy<FastqWriter> single = new Lazy<FastqWriter>(new Lazy.LazyInitializer<FastqWriter>() {
+
+			@Override
+			public FastqWriter make() {
+				return FACTORY
+					.newWriter(new File(IOUtils.makeOutputNameFastqWithDefaults(prefix + "_SE", GZIP_OUTPUT)));
+			}
+		});
+		return new PairFastqWriters(pair1, pair2, single);
+	}
+
+	/**
+	 * Create a split writer by barcode
+	 *
+	 * @param prefix     the prefix for the files
+	 * @param dictionary the barcode dictionary with the barcodes
+	 * @param paired     <code>true</code> indicates that paired writers are used; otherwise a default is used
+	 *
+	 * @return a new instance of the writer
+	 */
+	public SplitFastqWriter newSplitByBarcodeWriter(String prefix, BarcodeDictionary dictionary, final boolean paired) {
+		logger.debug("Creating new SplitByBarcode barcode for ", (paired) ? "paired" : "single", "-end");
+		final Hashtable<String, FastqWriter> mapping = new Hashtable<>();
+		HashMap<String, FastqWriter> sampleNames = new HashMap<>();
+		for (int i = 0; i < dictionary.numberOfSamples(); i++) {
+			String sample = dictionary.getSampleNames().get(i);
+			if (!sampleNames.containsKey(sample)) {
+				FastqWriter sampleWriter = (paired) ?
+					newPairWriter(prefix + "_" + sample) :
+					newWriter(prefix + "_" + sample);
+				sampleNames.put(sample, sampleWriter);
+			}
+			mapping.put(dictionary.getCombinedBarcodesFor(i), sampleNames.get(sample));
+		}
+		// add a unknow barcode
+		mapping.put(BarcodeMethods.UNKNOWN_STRING,
+			(paired) ? newPairWriter(prefix + "_" + DISCARDED_SUFFIX) : newWriter(prefix + "_" + DISCARDED_SUFFIX));
+		return new SplitFastqWriterAbstract(mapping) {
+
+			private final boolean p = paired;
+
+			@Override
+			public Hashtable<String, Object> getCurrentReport() {
+				if (!p) {
+					return new Hashtable<>(counts);
+				} else {
+					Hashtable<String, Object> report = new Hashtable<>();
+					for (Map.Entry<String, ? extends FastqWriter> entry : mapping.entrySet()) {
+						report.put(entry.getKey(), entry.getValue().toString());
+					}
+					return report;
+				}
+			}
+
+			@Override
+			public void write(FastqRecord record) {
+				String barcode = FastqRecordUtils.getBarcodeInName(record);
+				write(getUnknownIfNoMapping(barcode), record);
+			}
+
+			@Override
+			public void write(FastqPairedRecord record) {
+				String barcode = FastqRecordUtils.getBarcodeInName(record);
+				write(getUnknownIfNoMapping(barcode), record);
+			}
+
+			/**
+			 * All unknown barcodes (exact match) goes to the discarded
+			 * @param barcode the barcode to test if is present
+			 * @return the barcode if it is present, the UNKNOWN_STRING otherwise
+			 */
+			private String getUnknownIfNoMapping(String barcode) {
+				return (mapping.contains(barcode)) ? barcode : BarcodeMethods.UNKNOWN_STRING;
+			}
+		};
+	}
+
+	/**
+	 * Create a single-end split writer by barcode
+	 *
+	 * @param prefix     the prefix for the files
+	 * @param dictionary the barcode dictionary with the barcodes
+	 *
+	 * @return a new instance of the writer
+	 */
+	public SplitFastqWriter newSplitByBarcodeWriterSingle(String prefix, BarcodeDictionary dictionary) {
+		return newSplitByBarcodeWriter(prefix, dictionary, false);
+	}
+
+	/**
+	 * Create a pair-end split writer by barcode
+	 *
+	 * @param prefix     the prefix for the files
+	 * @param dictionary the barcode dictionary with the barcodes
+	 *
+	 * @return a new instance of the writer
+	 */
+	public SplitFastqWriter newSplitByBarcodeWriterPaired(String prefix, BarcodeDictionary dictionary) {
+		return newSplitByBarcodeWriter(prefix, dictionary, true);
+	}
+
+	/**
+	 * Writer that split between assign/unknow barcodes; the mapping is "assign" and {@link
+	 * org.vetmeduni.methods.barcodes.BarcodeMethods#UNKNOWN_STRING}. By default, any record is correct unless the
+	 * unknow string is provided as identifier
+	 *
+	 * @param prefix the prefix for the files
+	 * @param paired <code>true</code> indicates that paired writers are used; otherwise a default is used
+	 *
+	 * @return a new instance of the writer
+	 */
+	public SplitFastqWriter newSplitAssingUnknownBarcodeWriter(String prefix, boolean paired) {
+		logger.debug("Creating new Assing-Unknown barcode for ", (paired) ? "paired" : "single", "-end");
+		final Hashtable<String, FastqWriter> mapping = new Hashtable<>(2);
+		mapping.put("assign", (paired) ? newPairWriter(prefix) : newWriter(prefix));
+		mapping.put(BarcodeMethods.UNKNOWN_STRING,
+			(paired) ? newPairWriter(prefix + "_" + DISCARDED_SUFFIX) : newWriter(prefix + "_" + DISCARDED_SUFFIX));
+		return new SplitFastqWriterAbstract(mapping) {
+
+			private final boolean p = paired;
+
+			@Override
+			public Hashtable<String, Object> getCurrentReport() {
+				if (!p) {
+					return new Hashtable<>(counts);
+				} else {
+					Hashtable<String, Object> report = new Hashtable<>();
+					for (Map.Entry<String, ? extends FastqWriter> entry : mapping.entrySet()) {
+						report.put(entry.getKey(), entry.getValue().toString());
+					}
+					return report;
+				}
+			}
+
+			/**
+			 * By default, any record is correct
+			 * @param record the record to write
+			 *
+			 */
+			@Override
+			public void write(FastqRecord record) {
+				super.write("assign", record);
+			}
+
+			/**
+			 * By default, any record is correct
+			 * @param record the record to write
+			 *
+			 */
+			@Override
+			public void write(FastqPairedRecord record) {
+				super.write("assign", record);
+			}
+
+			@Override
+			public void write(String identifier, FastqRecord record) {
+				if (BarcodeMethods.UNKNOWN_STRING.equals(identifier)) {
+					super.write(identifier, record);
+				} else {
+					write(record);
+				}
+			}
+
+			@Override
+			public void write(String identifier, FastqPairedRecord record) {
+				if (BarcodeMethods.UNKNOWN_STRING.equals(identifier)) {
+					super.write(identifier, record);
+				} else {
+					write(record);
+				}
+			}
+		};
+	}
+
+	/**
+	 * Writer that split between assign/unknow barcodes for single-end
+	 *
+	 * @param prefix the prefix for the files
+	 *
+	 * @return a new instance of the writer
+	 */
+	public SplitFastqWriter newSplitAssingUnknownBarcodeWriterSingle(String prefix) {
+		return newSplitAssingUnknownBarcodeWriter(prefix, false);
+	}
+
+	/**
+	 * Writer that split between assign/unknow barcodes for single-end
+	 *
+	 * @param prefix the prefix for the files
+	 *
+	 * @return a new instance of the writer
+	 */
+	public SplitFastqWriter newSplitAssingUnknownBarcodeWriterPaired(String prefix) {
+		return newSplitAssingUnknownBarcodeWriter(prefix, true);
+	}
+}
