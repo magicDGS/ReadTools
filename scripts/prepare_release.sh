d2d7cd59f8b157c418e5e64f3c650286d60c0499
Daniel Gómez-Sánchez <magicDGS@users.noreply.github.com>
2017-05-18 12:29:56 +0200

    Release 1.0.0 (#235)
    Release version 1.0.0, including some functionality regarding the releasing process:
    
    * Use gitVerstion tag for a more verbose way of distinguish developmental versions
    * Add a release script to generate the tag and generate the javadoc
    * Update the current javadoc and doc pages version (required for correct display)
    * Update README to clarify that the SemVer refers to user-functionality

---
+++ scripts/prepare_release.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# exit on any failure
+set -e
+
+# TODO: parse the version from CHANGELOG?
+if [ -z "$1" ]; then
+	echo "Should provide a version..."
+	exit 1
+else
+	version=$1
+fi
+
+## cheks if the repository is clean
+if [[ ! -z "$(git status -s)" ]]; then
+	echo "Release requires a clean repository";
+	exit 2
+fi
+
+# TODO: Update version in CHANGELOG and docs/_data/sidebars/home_sidebar.yml
+echo "WARNING: CHANGELOG and home_sidebar are not updated!!"
+
+
+## start creating the relase
+echo "Create release for version $version (from master branch)"
+git checkout master
+git branch release_${version}
+git checkout release_${version}
+
+# tagging as version
+git tag $version
+
+# now generate the documentation
+echo "Generate javadoc and jar"
+./gradlew -Drelease=true clean javadoc currentJar &> tmp/release_${version}.gradle_output
+rm -fr docs/javadoc && mv build/docs/javadoc docs/
+
+git commit -am "Release javadoc"
+
+# TODO: generates the documentation with Barclay (https://github.com/magicDGS/ReadTools/issues/182)
