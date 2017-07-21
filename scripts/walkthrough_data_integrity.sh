cfcdb505895d7dd1392a6219ffd65653f99c90a6
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2017-07-21 11:41:43 +0200

    Fix bash script syntax

--- scripts/walkthrough_data_integrity.sh
+++ scripts/walkthrough_data_integrity.sh
@@ -5,28 +5,27 @@
 current_dir=${PWD}
 cd $(dirname $0)/../docs/walkthrough/data
 
-status=0
-echo "[`date`] Scanning walkthrough files"
+echo "[$(date)] Scanning walkthrough files"
 
 untracked=0
 corrupted=0
 missing=0
 
-# first iterate over the files in the 
-for n in `ls * | grep -v checksum`; do
-	grep_result=`grep " ${n}$" checksum`
+# first iterate over the files in the folder
+for n in $(ls * | grep -v checksum); do
+	grep_result=$(grep " ${n}$" checksum)
 	if [ "$grep_result" == "" ]; then
 		echo "+ Untracked data: $n"
 		untracked=$((untracked+1))
 	fi
 done
 
-# 
+# now iterate over the lines in the checksum file
 while read line; do
-	file=`echo $line | awk '{print $2}'`
+	file=$(echo $line | awk '{print $2}')
 	if [ -f $file ]; then
-		previous=`echo $line | awk '{print $1}'`
-		current=`md5sum $file | awk '{print $1}'`
+		previous=$(echo $line | awk '{print $1}')
+		current=$(md5sum $file | awk '{print $1}')
 		if [ "$previous" != "$current" ]; then
 			corrupted=$((corrupted+1))
 			echo "* Corrupted data: $line (now $current)"
@@ -37,8 +36,7 @@ while read line; do
 	fi
 done < checksum
 
-echo "[`date`] Data integrity: $untracked untracked, $missing missing, $corrupted corrupted files"
-
+echo "[$(date)] Data integrity: $untracked untracked, $missing missing, $corrupted corrupted files"
 
 cd $current_dir
 exit $((untracked + missing + corrupted))
