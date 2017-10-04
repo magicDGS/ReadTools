86c005384d05c6350d92c6fd54692e377e7a58ac
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2017-10-04 12:12:05 +0200

    Add minor fix to scripts/prepare_release.sh

--- scripts/prepare_release.sh
+++ scripts/prepare_release.sh
@@ -19,33 +19,35 @@ fi
 
 
 ## start creating the relase
-echo "Create release for version $version (from master branch)"
+echo "[$(date)] Create release for version $version (from master branch)"
 git checkout master
 git branch release_${version}
 git checkout release_${version}
+git push --set-upstream origin release_${version}
 
 # tagging as version
 git tag $version
 
 ## generate jar for upload
-echo "Generate jar"
+echo "[$(date)] Generate jar"
 ./gradlew -Drelease=true clean currentJar &> tmp/release_${version}.currentJar.out
 
 # now generate the documentation
-echo "Generate javadoc"
+echo "[$(date)] Generate javadoc"
 ./gradlew -Drelease=true javadoc &> tmp/release_${version}.javadoc.out
-rm -fr docs/javadoc && mv build/docs/javadoc docs/
-
-## commit and push
-git commit -am "Release javadoc" && git push
 
 ## generate the online documentation
-echo "Generate documentation site"
+echo "[$(date)] Generate documentation site"
 ./gradlew -Drelease=true readtoolsDoc &> tmp/release_${version}.readtoolsDoc.out
+
+## and copy to the new locations - no SNAPSHOT
+rm -fr docs/javadoc && mv build/docs/javadoc docs/
 mv build/docs/readtools/*.yml docs/_data/ && rm -fr docs/readtools/* && mv build/docs/readtools/*.md docs/readtools/
 
 ## commit and push
-git commit -am "Release documentation site" && git push
+echo "[$(date)] Upload to GitHub"
+git add docs/javadoc && git commit -m "Release javadoc" && git push
+git add docs/ && git commit -am "Release documentation site" && git push
 
 
 # TODO: Update version in CHANGELOG
