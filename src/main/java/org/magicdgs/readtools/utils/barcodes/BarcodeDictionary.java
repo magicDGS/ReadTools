5692d1aa59b2a64b2b91b556b35cdeb0142de29c
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2017-10-09 16:23:47 +0200

    Implement some methods of BarcodeDictionary

--- src/main/java/org/magicdgs/readtools/utils/barcodes/BarcodeDictionary.java
+++ src/main/java/org/magicdgs/readtools/utils/barcodes/BarcodeDictionary.java
@@ -25,49 +25,112 @@
 package org.magicdgs.readtools.utils.barcodes;
 
 import htsjdk.samtools.SAMReadGroupRecord;
+import org.broadinstitute.hellbender.utils.Utils;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Daniel Gomez-Sanchez (magicDGS)
  */
 public class BarcodeDictionary {
 
+    /**
+     * Value associated with unknown Read Groups (no index sequence).
+     * @see #getUnknownReadGroup()
+     */
+    public final String[] UNKNOWN_INDEX_SEQUENCE = new String[0];
 
-    public int getNumerOfIndexes() {
-        // TODO: implement
-        return 0;
+    // this is a LinkedHashMap on construction, so we know that the elements are sorted
+    private final Map<SAMReadGroupRecord, String[]> readGroupIndexes = new LinkedHashMap<>();
+    private final SAMReadGroupRecord unknownReadGroup;
+    private final int numberOfIndexes;
+
+    // TODO - we can also have a constructor from a SAMFileHeader with RG
+    // TODO - if https://github.com/samtools/hts-specs/issues/249 is added to the specs, we can build from SAMFileHeader
+    private BarcodeDictionary(final List<SAMReadGroupRecord> readGroups,
+            final List<String[]> readGroupIndexes,
+            final SAMReadGroupRecord unknownReadGroup) {
+        // non-null args and initialize
+        this.unknownReadGroup = Utils.nonNull(unknownReadGroup, "null unknown read group");
+
+        // validate sizes
+        Utils.validateArg(readGroups.size() == readGroupIndexes.size(), () -> "unequal sizes for read groups and indexes");
+        // handling the first sample to get the number of indexes for validation
+        final Iterator<String[]> indexIterator = readGroupIndexes.iterator();
+        final String[] first = indexIterator.next();
+        this.numberOfIndexes = first.length;
+        this.readGroupIndexes.put(readGroups.get(0),first);
+
+        // now iterating over the indexes
+        for (int i = 1; i < readGroups.size(); i++) {
+            final String[] indexes = readGroupIndexes.get(i);
+            Utils.validateArg(this.numberOfIndexes == indexes.length, () -> "no-matching number of indexes");
+            this.readGroupIndexes.put(readGroups.get(i), indexes);
+        }
+    }
+
+
+    public int getNumberOfIndexes() {
+        return numberOfIndexes;
     }
 
     public int getNumberOfSamples() {
-        // TODO: maybe cached? unless isn't necessary
         return getReadGroups().size();
     }
 
+    /**
+     * Gets the sample Read Groups in the barcode dictionary.
+     *
+     * @return the list of sample read groups in the dictionary.
+     */
     public List<SAMReadGroupRecord> getReadGroups() {
-        // TODO: implement
-        return null;
+        // TODO - maybe cache on construction?
+        return new ArrayList<>(readGroupIndexes.keySet());
     }
 
-    // TODO: maybe change signature
-    public String[] getReadGroupIndexSequences(final SAMReadGroupRecord record) {
-        // TODO: implement
-        return null;
+    // TODO: probably remove
+    public SAMReadGroupRecord getReadGroupAt(final int index) {
+        return getReadGroups().get(index);
     }
 
-    // TODO: maybe change signature
-    public List<String> getIndexSequences(final int index) {
-        // TODO: implement
-        return null;
+    public SAMReadGroupRecord getUnknownReadGroup() {
+        return unknownReadGroup;
+    }
+
+    /**
+     * Gets the index sequences for the Read Group.
+     * @param readGroup the requested Read Group.
+     * @return index sequence(s) from the Read Group; {@link #UNKNOWN_INDEX_SEQUENCE} if not found.
+     */
+    public String[] getReadGroupIndexSequences(final SAMReadGroupRecord readGroup) {
+        return readGroupIndexes.getOrDefault(readGroup, UNKNOWN_INDEX_SEQUENCE);
+    }
+
+    /**
+     * Gets all the index sequences at index {@code i} (0-based).
+     * @param i the requested index.
+     * @return list with the index sequences at index {@code i}.
+     */
+    public List<String> getIndexSequences(final int i) {
+        Utils.validIndex(i, numberOfIndexes);
+        // TODO - maybe it should be cached to improve efficiency!
+        return readGroupIndexes.values().stream().map(seqs -> seqs[i])
+                .collect(Collectors.toList());
     }
 
     public Set<String> getUniqueIndexSequences(final int index) {
-        // TODO: implement
+        // TODO - maybe it should be cached to improve efficiency
         return null;
     }
 
     public boolean isIndexUnique(final int index, final String barcodeSequence) {
-        return false;
+        return Collections.frequency(getIndexSequences(index), barcodeSequence) != 1;
     }
 }
