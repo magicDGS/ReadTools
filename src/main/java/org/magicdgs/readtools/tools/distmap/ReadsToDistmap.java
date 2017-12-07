92fd0c77188557ef228f23bc1321e0f46cb4df3b
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2017-12-07 10:40:46 +0100

    Add trimming support to ReadsToDistmap (no tests)

--- src/main/java/org/magicdgs/readtools/tools/distmap/ReadsToDistmap.java
+++ src/main/java/org/magicdgs/readtools/tools/distmap/ReadsToDistmap.java
@@ -27,27 +27,41 @@ package org.magicdgs.readtools.tools.distmap;
 import org.magicdgs.readtools.RTHelpConstants;
 import org.magicdgs.readtools.cmd.RTStandardArguments;
 import org.magicdgs.readtools.cmd.argumentcollections.FixBarcodeAbstractArgumentCollection;
+import org.magicdgs.readtools.cmd.plugin.TrimmerPluginDescriptor;
 import org.magicdgs.readtools.cmd.programgroups.DistmapProgramGroup;
 import org.magicdgs.readtools.engine.ReadToolsWalker;
 import org.magicdgs.readtools.tools.conversion.StandardizeReads;
 import org.magicdgs.readtools.utils.distmap.DistmapException;
 import org.magicdgs.readtools.utils.read.ReadWriterFactory;
+import org.magicdgs.readtools.utils.trimming.TrimAndFilterPipeline;
 
 import org.broadinstitute.barclay.argparser.Advanced;
 import org.broadinstitute.barclay.argparser.Argument;
 import org.broadinstitute.barclay.argparser.ArgumentCollection;
+import org.broadinstitute.barclay.argparser.CommandLineException;
+import org.broadinstitute.barclay.argparser.CommandLineParser;
+import org.broadinstitute.barclay.argparser.CommandLinePluginDescriptor;
 import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
 import org.broadinstitute.barclay.help.DocumentedFeature;
+import org.broadinstitute.hellbender.cmdline.GATKPlugin.GATKReadFilterPluginDescriptor;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
 import org.broadinstitute.hellbender.exceptions.UserException;
 import org.broadinstitute.hellbender.utils.read.GATKRead;
 import org.broadinstitute.hellbender.utils.read.GATKReadWriter;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Converts to the Distmap format
  * (<a href="http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0072614">
  * Pandey &amp; Schlötterer 2013</a>) any kind of ReadTools source, including information
  * from the barcodes (BC tag) in the read name (Illumina format) to allow keeping sample data.
+ *
+ * <p>If requested, it also applies a trimming/filtering pipeline to the reads.
+ * </p>
  *
  * @author Daniel Gomez-Sanchez (magicDGS)
  */
@@ -81,10 +95,39 @@ public final class ReadsToDistmap extends ReadToolsWalker {
     // the writer for the reads
     private GATKReadWriter writer;
 
+    // pipeline to trim and filter - if null, no trimming will be performed
+    private TrimAndFilterPipeline pipeline;
+
+    // track number of discarded single/pair-ends
+    private long discarded = 0;
+
+    @Override
+    public List<? extends CommandLinePluginDescriptor<?>> getPluginDescriptors() {
+        return Arrays.asList(
+                // no default trimmers
+                new TrimmerPluginDescriptor(new DistmapTrimmerPluginArgumentCollection(), Collections.emptyList()),
+                new GATKReadFilterPluginDescriptor(
+                        new DistmapFilterPluginArgumentCollection(),
+                        // no default filters
+                        Collections.emptyList())
+        );
+    }
+
     /** Validates the barcode-fixing arguments, and initializes the writer. */
     @Override
     public void onTraversalStart() {
         fixBarcodeTags.validateArguments();
+        // TODO - activate trimming
+        try {
+            final CommandLineParser parser = getCommandLineParser();
+            pipeline = TrimAndFilterPipeline.fromPluginDescriptors(
+                    parser.getPluginDescriptor(TrimmerPluginDescriptor.class),
+                    parser.getPluginDescriptor(GATKReadFilterPluginDescriptor.class)
+            );
+        } catch (CommandLineException.BadArgumentValue e) {
+            pipeline = null;
+        }
+
         writer = new ReadWriterFactory()
                 .setForceOverwrite(forceOverwrite)
                 .setHdfsBlockSize(blockSize)
@@ -94,12 +137,55 @@ public final class ReadsToDistmap extends ReadToolsWalker {
     /** Fixes the barcode tag(s) and write the read down. */
     @Override
     protected void apply(final GATKRead read) {
-        // we do not require an special handling of pair-end reads because the writer
-        // is doing it for us
-        // in addition, we do not have to assign the barcodes for the second pair, because
-        // they should have the same and we assume that the first one includes it
-        // see https://github.com/magicDGS/ReadTools/issues/159 for more detail
-        writer.addRead(fixBarcodeTags.fixBarcodeTags(read));
+        if (testRead(read)) {
+            writer.addRead(fixBarcodeTags.fixBarcodeTags(read));
+        } else {
+            discarded++;
+        }
+    }
+
+    @Override
+    protected void apply(final Tuple2<GATKRead, GATKRead> pair) {
+        final boolean firstPass = testRead(pair._1);
+        final boolean secondPass = testRead(pair._2);
+
+        // only if they pass, fix the barcode tags and write
+        if (firstPass && secondPass) {
+            fixBarcodeTags.fixBarcodeTags(pair);
+            writer.addRead(pair._1);
+            writer.addRead(pair._2);
+        } else {
+            discarded++;
+        }
+    }
+
+    @Override
+    public Object onTraversalSuccess() {
+        if (pipeline != null) {
+            final String pairedString = isPaired() ? "read pairs" : "reads";
+            logger.info("After trimming, {} {} were discarded.", discarded, pairedString);
+            pipeline.getTrimmingStats().forEach(stat -> {
+                logger.info("{}: {}/{} 5'-trimmed {}",
+                        stat.TRIMMER, stat.TRIMMED_5_P, stat.TOTAL, pairedString);
+                logger.info("{}: {}/{} 3'-trimmed {}",
+                        stat.TRIMMER, stat.TRIMMED_3_P, stat.TOTAL, pairedString);
+                logger.info("{}: {}/{} completely trimmed {}",
+                        stat.TRIMMER, stat.TRIMMED_COMPLETE, stat.TOTAL, pairedString);
+            });
+            pipeline.getFilterStats().forEach(stat ->
+                    logger.info("{}: {}/{} passing {} after filtering",
+                            stat.FILTER, stat.PASSED, stat.TOTAL, pairedString)
+            );
+        }
+        return null;
+    }
+
+    // test that the read is trimmed or not
+    private boolean testRead(final GATKRead read) {
+        if (pipeline != null) {
+            return pipeline.test(read);
+        }
+        return true;
     }
 
     /** Close the writer. */
