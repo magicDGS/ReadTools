4c4ebaa0373c1c85ce24021550903ba3d4d638cc
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2018-02-01 17:32:17 +0100

    scripts/

--- scripts/prepare_release.sh
+++ scripts/prepare_release.sh
@@ -3,7 +3,6 @@
 # exit on any failure
 set -e
 
-# TODO: parse the version from CHANGELOG?
 if [ -z "$1" ]; then
 	echo "Should provide a version..."
 	exit 1
@@ -45,14 +44,13 @@ rm -fr docs/javadoc && mv build/docs/javadoc docs/
 mv build/docs/readtools/*.yml docs/_data/ && rm -fr docs/readtools/* && mv build/docs/readtools/*.md docs/readtools/
 
 echo "[$(date)] Update CHANGELOG version"
-awk -v var=$version '{print $0}; $0=="## [Unreleased]"{print "\n\n## ["var"]"}END{print "["var"]: https://github.com/magicDGS/ReadTools/releases/tag/"var}' CHANGELOG.md > CHANGELOG.md.new
-mv CHANGELOG.md.new CHANGELOG.md
+awk -v var=$version '{print $0}; $0=="## [Unreleased]"{print "\n\n## ["var"]"}END{print "["var"]: https://github.com/magicDGS/ReadTools/releases/tag/"var}' CHANGELOG.md > CHANGELOG.md.new && mv CHANGELOG.md.new CHANGELOG.md
 
 ## commit and push
 echo "[$(date)] Upload to GitHub"
 git add docs/javadoc && git commit -m "Release javadoc" && git push
-git add docs/ && git commit -am "Release documentation site" && git push
-git add CHANGELOG.md && git commit -am "Update CHANGELOG" && git push
+git add docs/ && git commit -m "Release documentation site" && git push
+git add CHANGELOG.md && git commit -m "Update CHANGELOG" && git push
 
 echo "Please, upload to the release page the following file(s):"
 echo "* build/libs/ReadTools.jar"
