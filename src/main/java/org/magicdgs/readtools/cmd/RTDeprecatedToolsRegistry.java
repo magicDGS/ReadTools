954d78da489f3f4f79c4f51d65cd0ea0e35213ea
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2018-08-05 13:03:36 +0200

    Add deprecated tools registry to report legacy tools substitutes

---
+++ src/main/java/org/magicdgs/readtools/cmd/RTDeprecatedToolsRegistry.java
@@ -0,0 +1,64 @@
+package org.magicdgs.readtools.cmd;
+
+import org.magicdgs.readtools.RTHelpConstants;
+import org.magicdgs.readtools.tools.barcodes.AssignReadGroupByBarcode;
+import org.magicdgs.readtools.tools.conversion.ReadsToFastq;
+import org.magicdgs.readtools.tools.conversion.StandardizeReads;
+import org.magicdgs.readtools.tools.quality.QualityEncodingDetector;
+import org.magicdgs.readtools.tools.trimming.TrimReads;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Deprecated tool registry to inform for removal of tools in following versions.
+ *
+ * <p>Note: based on GATK's {@link org.broadinstitute.hellbender.cmdline.DeprecatedToolsRegistry}.
+ *
+ * @author Daniel Gomez-Sanchez (magicDGS)
+ */
+public final class RTDeprecatedToolsRegistry {
+
+    private RTDeprecatedToolsRegistry() {}
+
+    // Mapping from tool name to string describing the major version number where the tool first disappeared and
+    // optional recommended alternatives
+    private static Map<String, Pair<String, String>> deprecatedTools = null;
+
+    private static final String LEGACY_PREFIX = "Pre-release tool substituted by ";
+
+    private static Map<String, Pair<String, String>> getDeprecatedTools() {
+        if (deprecatedTools == null) {
+            deprecatedTools = new HashMap<>();
+            // legacy tools
+            deprecatedTools.put("QualityChecker", Pair.of("1.0.0", LEGACY_PREFIX + QualityEncodingDetector.class.getSimpleName()));
+            deprecatedTools.put("StandardizeQuality", Pair.of("1.0.0", LEGACY_PREFIX + StandardizeReads.class.getSimpleName()));
+            deprecatedTools.put("BamBarcodeDetector", Pair.of("1.0.0", LEGACY_PREFIX + AssignReadGroupByBarcode.class.getSimpleName()));
+            deprecatedTools.put("FastqBarcodeDetector", Pair.of("1.0.0", LEGACY_PREFIX + AssignReadGroupByBarcode.class.getSimpleName()));
+            deprecatedTools.put("TaggedBamToFastq", Pair.of("1.0.0", LEGACY_PREFIX + ReadsToFastq.class.getSimpleName()));
+            deprecatedTools.put("TrimFastq", Pair.of("1.0.0", LEGACY_PREFIX + TrimReads.class.getSimpleName()));
+
+        }
+        return deprecatedTools;
+    }
+
+    /**
+     * Utility method to pull up the version number at which a tool was deprecated and the suggested replacement, if any
+     *
+     * @param toolName   the tool class name (not the full package) to check
+     */
+    public static String getToolDeprecationInfo(final String toolName) {
+        final Pair<String, String> info = getDeprecatedTools().get(toolName);
+        return info != null ?
+                String.format("%s is no longer included in %s as of version %s. %s",
+                        toolName,
+                        RTHelpConstants.PROGRAM_NAME,
+                        info.getLeft(),
+                        info.getRight()
+                ) :
+                null;
+    }
+
+}
---
+++ src/test/java/org/magicdgs/readtools/cmd/RTDeprecatedToolsRegistryIntegrationTest.java
@@ -0,0 +1,48 @@
+package org.magicdgs.readtools.cmd;
+
+import org.magicdgs.readtools.Main;
+import org.magicdgs.readtools.RTBaseTest;
+
+import org.broadinstitute.hellbender.exceptions.UserException;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author Daniel Gomez-Sanchez (magicDGS)
+ */
+public class RTDeprecatedToolsRegistryIntegrationTest extends RTBaseTest {
+
+    // legacy tools included in pre-release (<= 0.3.0)
+    private final List<String> legacyTools = Arrays.asList(
+            "QualityChecker", "StandardizeQuality",
+            "BamBarcodeDetector", "FastqBarcodeDetector",
+            "TaggedBamToFastq", "TrimFastq");
+    private final String firstMajorVersion = "1.0.0";
+
+    @DataProvider
+    public Iterator<Object[]> getLegacyToolNames() {
+        return legacyTools.stream().map(s -> new Object[] {s}).iterator();
+    }
+
+    @Test(dataProvider = "getLegacyToolNames")
+    public void testLegacyTool(final String toolName) {
+        final UserException e = Assert.expectThrows(
+                UserException.class,
+                () -> new Main().instanceMain(new String[] {toolName})
+        );
+
+        // should contain the first major version message
+        Assert.assertTrue(e.getMessage().contains(firstMajorVersion), e.getMessage());
+        Assert.assertEquals(e.getMessage(), RTDeprecatedToolsRegistry.getToolDeprecationInfo(toolName));
+    }
+
+    @Test(dataProvider = "getLegacyToolNames")
+    public void testMisspelledLegacyTool(final String toolName) {
+        Assert.assertNull(RTDeprecatedToolsRegistry.getToolDeprecationInfo("L" + toolName));
+    }
+}
