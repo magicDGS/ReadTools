e5e2f21ee0f77fc6d9f2529acacf4ba98d72a9c7
magicDGS <daniel.gomez.sanchez@hotmail.es>
2018-08-27 12:59:05 +0200

    Fix unused imports

--- src/test/java/org/magicdgs/readtools/tools/distmap/ReadsToDistmapIntegrationTest.java
+++ src/test/java/org/magicdgs/readtools/tools/distmap/ReadsToDistmapIntegrationTest.java
@@ -43,9 +43,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author Daniel Gomez-Sanchez (magicDGS)
